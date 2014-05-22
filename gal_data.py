83532a4ccd83145e393d997ca834f219b2d13495
AKL <adam.leroy@gmail.com>
2014-05-22 08:04:27 -0400

    More fixes

---
+++ gal_data.py
@@ -0,0 +1,12 @@
+import astropy.io.fits as fits
+
+def gal_data(
+    name=None,
+    all=False,
+    data_dir=None,
+    found=None,
+    ):
+    """
+    """    
+    # http://astropy.readthedocs.org/en/latest/io/fits/#working-with-table-data
+    pass
