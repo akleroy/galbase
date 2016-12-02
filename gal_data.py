87bc6a9aa2c4143e62ba6d3b67035e0eda36c353
jiang696 <jiang696@gmail.com>
2016-12-02 10:06:14 -0800

    update gal_data.py

--- gal_data.py
+++ gal_data.py
@@ -1,80 +1,72 @@
-import astropy.io.fits as fits
-from astropy.table import Table
+import astropy.io.fits as pyfits
+import os
 import numpy as np
+from pdb import set_trace
 
-def gal_data(name_in=None, all_=False, tag=None):
-    """
-    DEFAULTS, DEFINITIONS, AND ERROR HANDLING
-    """
-    # CHECK THAT WE GOT A NAME
-    if (not name_in) and (all_ == False) and (not tag):
-        raise ValueError('Need a name to find a galaxy. Returning empty \
-                          structure')
-                          
-    # SET INPUTS TO NUMPY ARRAYS
-    name_in = np.array(name_in)
-    tag = np.array(tag)
-    
-    # DIRECTORY FOR THE DATABASE
-    data_dir = "gal_data/"
-    
-    """
-    READ THE DATA BASE
-    """
-    fname = data_dir + "gal_base.fits"
-    hdulist = fits.open(fname)
-    data = Table(hdulist[1].data)
-    hdulist.close()
-    n_data = len(data)
-    for i in range(n_data):
-        data['NAME'][i] = data['NAME'][i].strip(' ')
-        data['TAGS'][i] = data['TAGS'][i].strip(' ')
-        data['MORPH'][i] = data['MORPH'][i].strip(' ')
-    """
-    TREAT THE CASE WHERE A SURVEY OR ALL DATA ARE DESIRED
-    """
-    if all_:
+def gal_data(names=None, data=None, all=False, data_dir=None, tag=None):
+
+    if not names and not all and not tag:
+        print('Need a name to find a galaxy. Returning empty structure')
+        return None
+
+    if not data_dir:
+        galbase_dir, this_filename = os.path.split(__file__)
+        galdata_dir = os.path.join(galbase_dir, "gal_data")
+
+    # READ IN THE DATA
+    if data is None:
+        dbfile = os.path.join(galdata_dir, 'gal_base.fits')
+        hdulist = pyfits.open(dbfile)
+        data = hdulist[1].data
+        hdulist.close()
+
+
+    # ALL DATA ARE DESIRED
+    if all:
         return data
 
-    if tag:
-        keep = np.ones(n_data, dtype=bool)
-        for i in range(n_data):
-            this_tag = [temp for temp in data['TAGS'][i].split(";")]
-            for t in tag:
-                if t not in this_tag:
-                    keep[i] = False
-                    break
-        if sum(keep) == 0:
-            print('No targets found with that tag combination. Returning an \
-                   empty structure.')
-        return data[keep]
+    # A SPECIFIC SURVEY IS USED
+    if tag is not None:
+        n_data = len(data)
+        keep = np.ones(n_data)
+        # survey_file = os.path.join(galdata_dir, 'survey_' + tag.lower() + '.txt')
+        # gals = np.genfromtxt(survey_file, dtype='string')
 
-    """
-    TREAT THE CASE OF A NAME OR LIST OF NAMES
-    """
-    # Build the list of aliases
+        for i in range(n_data):
+            this_tag = data['tags'][i].strip(';').split(';;')
+            keep[i] = sum(np.in1d(tag, this_tag))
+
+        if np.sum(keep) == 0:
+            print('No targets found with that tag combination.')
+            return None
+
+        good_data = data[np.where(keep)]
+
+        return good_data
+
+    # BUILD ALIAS DICTIONARY
     aliases = {}
-    fname = data_dir + "gal_base_alias.txt"
+    fname = os.path.join(galdata_dir, "gal_base_alias.txt")
     f = open(fname)
     f.readline()
     f.readline()
     for line in f:
         s = [temp for temp in line.strip('\n').split(' ')]
-        aliases[s[0]] = s[1]
+        aliases[s[0].replace(' ', '').upper()] = s[1].replace(' ', '').upper()
 
-    # Create list for all names
-    names = []
-    for name in name_in:
-        try:
-            names.append(aliases[name])
-        except KeyError:
-            print(name, 'not in current alias list')
-    
-    # Look up the current data
-    keep = np.zeros(n_data, dtype=bool)
-    for i in range(n_data):
-        if data[i].field('NAME') in names:
-            keep[i] = True
+    # NAME OR NAMES of GALAXIES
+    if type(names) == str:
+        names = [names]
+    keep = np.zeros(len(data), dtype=bool)
 
-    # Return
+    # SEARCH FOR GALAXIES
+    for name in names:
+        name_a = aliases[name.replace(' ', '').upper()]
+        ind = data.field('NAME') == name_a
+
+        if sum(ind) == 0:
+            print('No match for ' + name)
+        else:
+            keep += ind
+
     return data[keep]
